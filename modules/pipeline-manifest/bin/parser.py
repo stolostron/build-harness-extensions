b6d23cdb27e0a0a51371d2baa30188f7eae995cd
David Schmidt <51931019+schmidtd@users.noreply.github.com>
2020-03-03 08:29:51 -0500

    Fail harder (#20)
    * Actually check the output of the retag targets
    
    * Parameterize the branch that does the quay retagging
    
    * Admit to what we're deleting
    
    * Pretty pretty

--- modules/pipeline-manifest/bin/parser.py
+++ modules/pipeline-manifest/bin/parser.py
@@ -10,4 +10,4 @@ for v in data:
     component_version = v["tag"].replace('-'+v["sha256"],'')
     retag_name = component_version + "-SNAPSHOT-" + sys.argv[2]
     run('echo QUAY_RETAG_NAME={} COMPONENT_NAME={} QUAY_COMPONENT_TAG={} QUAY_DRY_RUN={}'.format(retag_name,component_name,compenent_tag,sys.argv[3]), shell=True)
-    run('make quay/retag QUAY_RETAG_NAME={} COMPONENT_NAME={} QUAY_COMPONENT_TAG={} QUAY_DRY_RUN={}'.format(retag_name,component_name,compenent_tag,sys.argv[3]), shell=True)
+    run('make quay/retag QUAY_RETAG_NAME={} COMPONENT_NAME={} QUAY_COMPONENT_TAG={} QUAY_DRY_RUN={}'.format(retag_name,component_name,compenent_tag,sys.argv[3]), shell=True, check=True)
