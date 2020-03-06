32f76b9b73d36d03d18511286a2687fea08feb90
David Schmidt <51931019+schmidtd@users.noreply.github.com>
2020-03-06 08:41:15 -0500

    As part of tagging for snapshots, also create a git tag at the designated sha (#22)
    * Refactor retag module - initial push for testing
    
    * Track sha
    
    * Pull out the sha from the manifest
    
    * Try limiting git activity solely to helloworld
    
    * More better syntax
    
    * Let 'em loose
    
    * Who moved the token?
    
    * Be silenter with curl output, die on error
    
    * Further movement of the QUAY_TOKEN
    
    * Git repo is not the same a component name

--- modules/pipeline-manifest/bin/parser.py
+++ modules/pipeline-manifest/bin/parser.py
@@ -7,7 +7,10 @@ data = json.load(open(sys.argv[1]))
 for v in data:
     component_name = v["name"]
     compenent_tag = v["tag"]
+    compenent_sha = v["sha256"]
+    component_repo = v["repository"]
     component_version = v["tag"].replace('-'+v["sha256"],'')
     retag_name = component_version + "-SNAPSHOT-" + sys.argv[2]
-    run('echo QUAY_RETAG_NAME={} COMPONENT_NAME={} QUAY_COMPONENT_TAG={} QUAY_DRY_RUN={}'.format(retag_name,component_name,compenent_tag,sys.argv[3]), shell=True)
-    run('make quay/retag QUAY_RETAG_NAME={} COMPONENT_NAME={} QUAY_COMPONENT_TAG={} QUAY_DRY_RUN={}'.format(retag_name,component_name,compenent_tag,sys.argv[3]), shell=True, check=True)
+    run('echo RETAG_SNAPSHOT_NAME={} COMPONENT_NAME={} RETAG_REPO={} RETAG_QUAY_COMPONENT_TAG={} RETAG_GITHUB_SHA={} RETAG_DRY_RUN={}'.format(retag_name, component_repo, component_name, compenent_tag, compenent_sha, sys.argv[3]), shell=True)
+    run('make retag/git RETAG_SNAPSHOT_NAME={} RETAG_REPO={} RETAG_QUAY_COMPONENT_TAG={} RETAG_GITHUB_SHA={} RETAG_DRY_RUN={}'.format(retag_name, component_repo, compenent_tag, compenent_sha, sys.argv[3]), shell=True, check=True)
+    run('make retag/quay RETAG_SNAPSHOT_NAME={} COMPONENT_NAME={} RETAG_QUAY_COMPONENT_TAG={} RETAG_GITHUB_SHA={} RETAG_DRY_RUN={}'.format(retag_name, component_name, compenent_tag, compenent_sha, sys.argv[3]), shell=True, check=True)
