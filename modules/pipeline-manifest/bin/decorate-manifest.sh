6c0c06249dfd77f8e250c479bc3403e100359199
David Schmidt <51931019+schmidtd@users.noreply.github.com>
2020-04-27 10:25:47 -0400

    Decorate new-format of manifest (#51)

--- modules/pipeline-manifest/bin/decorate-manifest.sh
+++ modules/pipeline-manifest/bin/decorate-manifest.sh
@@ -3,6 +3,7 @@
 # Incoming variables:
 #   $1 - name of the manifest json file (should exist)
 #   $2 - name of the sha'd manifest json file (to be created)
+#   $3 - image-to-alias dictionary (image-alias.json, should exist)
 #
 # Required environment variables:
 #   $QUAY_TOKEN - you know, the token... to quay (needs to be able to read open-cluster-management stuffs
@@ -16,17 +17,19 @@ fi
 
 manifest_filename=$1
 new_filename=$2
+dictionary_filename=$3
 
 echo Incoming manfiest filename: $manifest_filename
 echo Creating shad manfiest filename: $new_filename
 
 rm manifest-sha.badjson 2> /dev/null
 cat $manifest_filename | jq -rc '.[]' | while IFS='' read item;do
-  name=$(echo $item | jq -r .name)
-  remote=$(echo $item | jq -r .remote)
-  repository=$(echo $item | jq -r .repository | awk -F"/" '{ print $1 }')
-  tag=$(echo $item | jq -r .tag)
-  #echo name: [$name] remote: [$remote] repostory: [$repository] tag: [$tag]
+  name=$(echo $item | jq -r '.["image-name"]')
+  remote=$(echo $item | jq -r '.["image-remote"]')
+  repository=$(echo $item | jq -r '.["git-repository"]' | awk -F"/" '{ print $1 }')
+  tag=$(echo $item | jq -r '.["image-tag"]')
+  image_key=$(jq -r --arg image_name $name '.[] | select (.["image-name"]==$image_name) | .["image-key"]' $dictionary_filename )
+ #echo name: [$name] remote: [$remote] repostory: [$repository] tag: [$tag]
   url="https://quay.io/api/v1/repository/$repository/$name/tag/?onlyActiveTags=true&specificTag=$tag"
   #echo $url
   curl_command="curl -s -X GET -H \"Authorization: Bearer $QUAY_TOKEN\" \"$url\""
@@ -38,7 +41,7 @@ cat $manifest_filename | jq -rc '.[]' | while IFS='' read item;do
     echo Oh no, can\'t retrieve sha from $url
     exit 1
   fi
-  echo $item | jq --arg sha_value $sha_value '. + { "manifest-sha256": $sha_value }' >> manifest-sha.badjson
+  echo $item | jq --arg sha_value $sha_value --arg image_key $image_key '. + { "image-digest": $sha_value, "image-key": $image_key }' >> manifest-sha.badjson
 done
 echo Creating $new_filename file
 jq -s '.' < manifest-sha.badjson > $new_filename
