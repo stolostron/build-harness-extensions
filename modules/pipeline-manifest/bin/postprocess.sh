a48638e067db325dc4c5c19538ed53a2a271664a
David Schmidt <51931019+schmidtd@users.noreply.github.com>
2020-05-01 09:19:52 -0400

    Allow for staggered invocation

--- modules/pipeline-manifest/bin/postprocess.sh
+++ modules/pipeline-manifest/bin/postprocess.sh
@@ -3,7 +3,7 @@
 # Incoming variables:
 #   $1 - name of the manifest json file (should exist)
 #   $2 - snapshot tag (the whole thing; i.e. 1.0.0-SNAPSHOT-2020-05-01-02-47-45)
-#
+#   $3 - sequence of events (1=endpoint operator, 2=hub operator)
 # Required environment variables:
 #   $QUAY_TOKEN - you know, the token... to quay (needs to be able to read open-cluster-management stuffs
 #
@@ -16,18 +16,26 @@ fi
 
 manifest_filename=$1
 TAG=$2
+ENDPOINT=$3
 
 echo Incoming manfiest filename: $manifest_filename
 echo Incoming tag: $TAG
+echo ENDPOINT: $ENDPOINT
 
-echo Before, $manifest_filename is:
-cat $manifest_filename
-
-ep_quaysha=`make -s retag/getquaysha RETAG_QUAY_COMPONENT_TAG=$TAG-dls COMPONENT_NAME=endpoint-operator`
-mco_quaysha=`make -s retag/getquaysha RETAG_QUAY_COMPONENT_TAG=$TAG-dls COMPONENT_NAME=multiclusterhub-operator`
-echo endpoint-operator quay sha: $ep_quaysha
-echo multiclusterhub-operator quay sha: $mco_quaysha
-jq --version
+if [[ "$ENDPOINT" == "1" ]]
+then
+  # The endpoint operator needs to exist first so later we can get its sha and insert that into hub operator
+  ep_quaysha=`make -s retag/getquaysha RETAG_QUAY_COMPONENT_TAG=$TAG-sha COMPONENT_NAME=endpoint-operator`
+  mco_quaysha=`make -s retag/getquaysha RETAG_QUAY_COMPONENT_TAG=$TAG COMPONENT_NAME=multiclusterhub-operator`
+  echo first round, endpoint-operator quay sha: $ep_quaysha
+  echo first round, multiclusterhub-operator quay sha: $mco_quaysha
+else
+  # Now that endpoint operator exists, grab its sha so we have that in the manifest to insert into hub operator
+  ep_quaysha=`make -s retag/getquaysha RETAG_QUAY_COMPONENT_TAG=$TAG-sha COMPONENT_NAME=endpoint-operator`
+  mco_quaysha=`make -s retag/getquaysha RETAG_QUAY_COMPONENT_TAG=$TAG-sha COMPONENT_NAME=multiclusterhub-operator`
+  echo second round, endpoint-operator quay sha: $ep_quaysha
+  echo second round, multiclusterhub-operator quay sha: $mco_quaysha
+fi
 jq --arg ep_quaysha $ep_quaysha '(.[] | select (.["image-name"] == "endpoint-operator") | .["image-digest"]) |= $ep_quaysha' $manifest_filename > tmp.json ; mv tmp.json $manifest_filename
 jq --arg mco_quaysha $mco_quaysha '(.[] | select (.["image-name"] == "multiclusterhub-operator") | .["image-digest"]) |= $mco_quaysha' $manifest_filename > tmp.json ; mv tmp.json $manifest_filename
 echo After, $manifest_filename is:
