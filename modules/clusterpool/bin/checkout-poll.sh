bdbb6101532fb9dbdab0f6498c1932fd5b83d6a5
David Schmidt <51931019+schmidtd@users.noreply.github.com>
2020-09-30 14:15:21 -0400

    Refined claim criteria (#96)
    * Refined claim criteria
    
    * Remove debug stuffs
    
    * Updates to cluster availability logic
    
    * Use bash, not sh
    
    * Switch syntax
    
    * Add debug
    
    * Remove conditional test for debug
    
    * Swap logic from negative to positive
    
    * Remove conditional altogether
    
    * Are we getting the cluster claim json at all?
    
    * More debug
    
    * More debug
    
    * Suppress some make output from jq's prying eyes
    
    * Suppress EVERYTHING
    
    * Suppress EVERYTHING
    
    * Remove debug

--- modules/clusterpool/bin/checkout-poll.sh
+++ modules/clusterpool/bin/checkout-poll.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 # Total "minutes" to retry before giving up
-CLUSTERPOOL_WAIT_MINUTES=$1
+CLUSTERPOOL_CHECKOUT_TIMEOUT_MINUTES=$1
+
+if [ "$CLUSTERPOOL_CHECKOUT_TIMEOUT_MINUTES" = "" ]; then CLUSTERPOOL_CHECKOUT_TIMEOUT_MINUTES=10
+fi
+
 # We retry on 30-second intervals (plus query overhead...)
-RETRIES=$(( 2*CLUSTERPOOL_WAIT_MINUTES ))
+RETRIES=$(( 2*$CLUSTERPOOL_CHECKOUT_TIMEOUT_MINUTES ))
 
 make clusterpool/_create-claim
-make clusterpool/_verify-claim -s  > .verifyStatus
-if [ "`cat .verifyStatus`" = "ClusterClaimed" ]; then cat .verifyStatus; else
-	if [ "`cat .verifyStatus`" = "NoClusters" ]; then
-		echo Waiting $CLUSTERPOOL_WAIT_MINUTES minutes for cluster availability...
+make clusterpool/_gather-status
+
+if [ "`cat .verifyStatus`" = "ClusterReady" ]; then cat .verifyStatus; else
+	if [ ! "`cat .verifyStatus`" = "ClusterReady" ]; then
+		echo Waiting $CLUSTERPOOL_CHECKOUT_TIMEOUT_MINUTES minutes for cluster availability...
+		cat .verifyStatus
 		for (( i=1; i<=$RETRIES; i++ ))
 		do
 			sleep 30
-			make clusterpool/_verify-claim -s > .verifyStatus
+			make clusterpool/_gather-status
 			cat .verifyStatus
-			if [ "`cat .verifyStatus`" = "ClusterClaimed" ]; then exit 0; fi
+			if [ "`cat .verifyStatus`" = "ClusterReady" ]; then exit 0; fi
 		done
 	else
 		echo Unknown claim status `cat .verifyStatus` - exiting
