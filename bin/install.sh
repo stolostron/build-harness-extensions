342a12ef450ee3c1b92e999742632aab4c79f0eb
Hanqiu Zhang <hanzhang@redhat.com>
2020-10-21 20:15:45 -0400

    remove github token for bootstrap

--- bin/install.sh
+++ bin/install.sh
@@ -3,8 +3,6 @@ export BUILD_HARNESS_ORG=${1:-cloudposse}
 export BUILD_HARNESS_PROJECT=${2:-build-harness}
 export BUILD_HARNESS_BRANCH=${3:-master}
 # These will need to be used if a private repo is going to be cloned
-export GITHUB_USER=${4}
-export GITHUB_TOKEN=${5}
 export GITHUB_REPO="https://github.com/${BUILD_HARNESS_ORG}/${BUILD_HARNESS_PROJECT}.git"
 
 if [ "$BUILD_HARNESS_PROJECT" ] && [ -d "$BUILD_HARNESS_PROJECT" ]; then
