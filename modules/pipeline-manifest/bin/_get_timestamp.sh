00fbed1a61f7e30ea3ef712d34857d39f2a83cf8
David Schmidt <51931019+schmidtd@users.noreply.github.com>
2021-01-12 02:46:23 -0500

    Add klusterlet index processing (#7349)

--- modules/pipeline-manifest/bin/_get_timestamp.sh
+++ modules/pipeline-manifest/bin/_get_timestamp.sh
@@ -3,5 +3,5 @@
 TEMPFILE=$1
 TEMPFILE2=$1''2
 
-timestamp=$(curl --silent --location -H "Authorization: Bearer $REDHAT_REGISTRY_TOKEN" https://registry.redhat.io/v2/rhacm2/acm-operator-bundle/manifests/$2 | jq '[.history[]]|map(.v1Compatibility|fromjson|.created)|sort|reverse|.[0]')
-jq '. + [ {"version": "'$2'", "timestamp": '$timestamp'}]' $TEMPFILE > $TEMPFILE2; mv $TEMPFILE2 $TEMPFILE
+timestamp=$(curl --silent --location -H "Authorization: Bearer $REDHAT_REGISTRY_TOKEN" https://registry.redhat.io/v2/rhacm2/$2/manifests/$3 | jq '[.history[]]|map(.v1Compatibility|fromjson|.created)|sort|reverse|.[0]')
+jq '. + [ {"version": "'$3'", "timestamp": '$timestamp'}]' $TEMPFILE > $TEMPFILE2; mv $TEMPFILE2 $TEMPFILE
--- modules/pipeline-manifest/bin/_mirror-index.sh
+++ modules/pipeline-manifest/bin/_mirror-index.sh
@@ -9,7 +9,7 @@
 #  5. Build our catalog and push it
 
 # Get logged into brew, update/check out the repos we need
-echo Preparing environment for release $Z_RELEASE_VERSION...
+echo Preparing environment for release $Z_RELEASE_VERSION tagged as $PIPELINE_MANFIEST_INDEX_IMAGE_TAG...
 OC=$BUILD_HARNESS_PATH/vendor/oc
 BIN_PATH=$BUILD_HARNESS_PATH/../build-harness-extensions/modules/pipeline-manifest/bin
 rm -rf /tmp/acm-custom-registry
@@ -30,7 +30,10 @@ if [[ ! -s .ashdod_output ]]; then
   echo No output from ashdod\; aborting
   exit 1
 fi
+echo "acm-operator-bundle tag:"
 cat .ashdod_output | grep "Image to mirror: acm-operator-bundle:" | awk -F":" '{print $3}' | tee .acm_operator_bundle_tag
+echo "klusterlet-operator-bundle tag:"
+cat .ashdod_output | grep "Image to mirror: klusterlet-operator-bundle:" | awk -F":" '{print $3}' | tee .klusterlet_operator_bundle_tag
 
 # Mirror the openshift images we depend on
 # Note: the oc image extract command is so dangerous that we ensure we are in a known-good location before attempting extraction
@@ -57,28 +60,53 @@ fi
 echo Locating upgrade bundles...
 docker login -u $PIPELINE_MANIFEST_REDHAT_USER -p $PIPELINE_MANIFEST_REDHAT_TOKEN registry.access.redhat.com
 export REDHAT_REGISTRY_TOKEN=$(curl --silent -u "$PIPELINE_MANIFEST_REDHAT_USER":$PIPELINE_MANIFEST_REDHAT_TOKEN "https://sso.redhat.com/auth/realms/rhcc/protocol/redhat-docker-v2/auth?service=docker-registry&client_id=curl&scope=repository:rhel:pull" | jq -r '.access_token')
-rm .extrabs
+rm .extrabs-acm
 
-# 0. Prepare for battle
-TEMPFILE=.extrabs.json
+# Prepare for battle
+TEMPFILE=.extrabs-acm.json
 TEMPFILE2=$TEMPFILE''2
 echo "[]" > $TEMPFILE
 
+BUNDLE=acm-operator-bundle
 # Extract version list, Pull out timestamp
-curl --silent --location -H "Authorization: Bearer $REDHAT_REGISTRY_TOKEN" https://registry.redhat.io/v2/rhacm2/acm-operator-bundle/tags/list | jq -r '.tags[] | select(test("'$PIPELINE_MANIFEST_BUNDLE_REGEX'"))' | xargs -L1 -I'{}' $BIN_PATH/_get_timestamp.sh $TEMPFILE {}
+curl --silent --location -H "Authorization: Bearer $REDHAT_REGISTRY_TOKEN" https://registry.redhat.io/v2/rhacm2/$BUNDLE/tags/list | jq -r '.tags[] | select(test("'$PIPELINE_MANIFEST_BUNDLE_REGEX'"))' | xargs -L1 -I'{}' $BIN_PATH/_get_timestamp.sh $TEMPFILE $BUNDLE {}
 
 # Sort results
 jq '. | sort_by(.["timestamp"])' $TEMPFILE > $TEMPFILE2; mv $TEMPFILE2 $TEMPFILE
 
-# Build the extrabs strucutre
-jq -r '.[].version' .extrabs.json | xargs -L1 -I'{}' echo  "-B registry.redhat.io/rhacm2/acm-operator-bundle:{}" > .extrabs
-
-export COMPUTED_UPGRADE_BUNDLES=$(cat .extrabs)
+# Build the extrabs strucutre for acm-operator-bundle
+jq -r '.[].version' $TEMPFILE | xargs -L1 -I'{}' echo  "-B registry.redhat.io/rhacm2/acm-operator-bundle:{}" > .extrabs-acm
+export COMPUTED_UPGRADE_BUNDLES=$(cat .extrabs-acm)
 echo Adding upgrade bundles:
 echo $COMPUTED_UPGRADE_BUNDLES
-
-# Build the catalog
+# Build a catalog from acm-operator-bundle
 cd release; echo tools/downstream-testing/build-catalog.sh $(cat ../.acm_operator_bundle_tag) $PIPELINE_MANFIEST_INDEX_IMAGE_TAG; tools/downstream-testing/build-catalog.sh $(cat ../.acm_operator_bundle_tag) $PIPELINE_MANFIEST_INDEX_IMAGE_TAG; cd ..
+
+if [[ "$PIPELINE_MANIFEST_RELEASE_VERSION" == "2.0" || "$PIPELINE_MANIFEST_RELEASE_VERSION" == "2.1" ]]; then echo No klusterlet index expected in version $PIPELINE_MANIFEST_RELEASE_VERSION;
+else
+  echo klusterlet index expected in version $PIPELINE_MANIFEST_RELEASE_VERSION
+
+  # Prepare for battle
+  TEMPFILE=.extrabs-klusterlet.json
+  TEMPFILE2=$TEMPFILE''2
+  echo "[]" > $TEMPFILE
+
+  BUNDLE=klusterlet-operator-bundle
+  # Extract version list, Pull out timestamp
+  curl --silent --location -H "Authorization: Bearer $REDHAT_REGISTRY_TOKEN" https://registry.redhat.io/v2/rhacm2/$BUNDLE/tags/list | jq -r '.tags[] | select(test("'$PIPELINE_MANIFEST_BUNDLE_REGEX'"))' | xargs -L1 -I'{}' build-harness-extensions/modules/pipeline-manifest/bin/_get_timestamp.sh $TEMPFILE $BUNDLE {}
+
+  # Sort results
+  jq '. | sort_by(.["timestamp"])' $TEMPFILE > $TEMPFILE2; mv $TEMPFILE2 $TEMPFILE
+
+  # Build the extrabs strucutre for klusterlet-operator-bundle
+  jq -r '.[].version' $TEMPFILE | xargs -L1 -I'{}' echo  "-B registry.redhat.io/rhacm2/klusterlet-operator-bundle:{}" > .extrabs-klusterlet
+  export COMPUTED_UPGRADE_BUNDLES=$(cat .extrabs-klusterlet)
+  echo Adding upgrade bundles:
+  echo $COMPUTED_UPGRADE_BUNDLES
+
+  # Build a catalog from klusterlet-operator-bundle
+  cd release; echo tools/downstream-testing/build-catalog.sh $(cat ../.klusterlet_operator_bundle_tag) $PIPELINE_MANFIEST_INDEX_IMAGE_TAG quay.io/acm-d/klusterlet-custom-registry quay.io/acm-d/klusterlet-operator-bundle; tools/downstream-testing/build-catalog.sh $(cat ../.klusterlet_operator_bundle_tag) $PIPELINE_MANFIEST_INDEX_IMAGE_TAG quay.io/acm-d/klusterlet-custom-registry quay.io/acm-d/klusterlet-operator-bundle; cd ..
+fi
 
 # Try to make the droppings writable - useful for shared machines
 chmod a+w -R /tmp/acm-custom-registry*
