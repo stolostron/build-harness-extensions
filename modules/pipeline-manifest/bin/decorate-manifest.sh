ce0c38b2fb26b9545f200e16b899222fbc8f4a55
David Schmidt <51931019+schmidtd@users.noreply.github.com>
2021-07-26 14:34:10 -0400

    Remove debug

--- modules/pipeline-manifest/bin/decorate-manifest.sh
+++ modules/pipeline-manifest/bin/decorate-manifest.sh
@@ -78,7 +78,7 @@ echo Creating $shad_filename file
 jq -s '.' < manifest-sha.badjson > $shad_filename
 rm manifest-sha.badjson 2> /dev/null
 
-if [[ -z "$instrumented_filename" ]]; then
+if [[ ! -z "$instrumented_filename" ]]; then
   echo Creating $instrumented_filename file
   jq -s '.' < manifest-instrumented.badjson > $instrumented_filename
   rm manifest-instrumented.badjson 2> /dev/null
