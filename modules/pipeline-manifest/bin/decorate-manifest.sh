141cfd5bfc9c0bda9763bde4f65358052424709d
David Schmidt <51931019+schmidtd@users.noreply.github.com>
2021-10-07 10:18:25 -0400

    Add tags together to help identify lineage

--- modules/pipeline-manifest/bin/decorate-manifest.sh
+++ modules/pipeline-manifest/bin/decorate-manifest.sh
@@ -50,7 +50,7 @@ cat $manifest_filename | jq -rc '.[]' | while IFS='' read item; do
     echo "**** Home ****"
   else
     echo "**** Away, mirroring to Home ****"
-    $OC image mirror $remote/$name:$tag=$home_quay_org/$name:$home_tag --keep-manifest-list=true --filter-by-os=.*
+    $OC image mirror $remote/$name:$tag=$home_quay_org/$name:$tag-$home_tag --keep-manifest-list=true --filter-by-os=.*
   fi
   echo image name: [$name] remote: [$remote] repostory: [$repository] tag: [$tag] image_key: [$image_key]
 
