04468df160fb82d3d246c6d0b1e1fd5e2437e089
Kyl-Bempah <24903083+Kyl-Bempah@users.noreply.github.com>
2023-06-07 13:49:52 -0400

    Add logging and naming fix

--- modules/pipeline-manifest/bin/_new-release-branch.sh
+++ modules/pipeline-manifest/bin/_new-release-branch.sh
@@ -25,30 +25,35 @@ teardown() {
 }
 
 community_creation() {
-    # $1 - pipeline
-    # $2 - pipeline x.y
-    # $3 - community x.y 
-    # $4 - community branch name
-    # $5 - communtiy tag name
-    git checkout main
-    git pull
-    git checkout -b $4-$3
-    touch TAG
-    touch RELEASE_VERSION
-    cat $3 > RELEASE_VERSION
-    touch Z_RELEASE_VERSION
-    cat $3.0 > Z_RELEASE_VERSION
-    if [ "$1" = "release" ];
-        touch ACM_Z_RELEASE_VERSION
-        cat $2.0 > ACM_Z_RELEASE_VERSION
-        touch STOLOSTRON_ENGINE_RELEASE_VERSION
-        cat $3 > STOLOSTRON_ENGINE_RELEASE_VERSION
-    elif [ "$1" = "backplane" ];
-        touch BACKPLANE_Z_RELEASE_VERSION
-        cat $2.0 > BACKPLANE_Z_RELEASE_VERSION
-    fi
-    git commit -am "Inital cutover for new branch"
-    git push --set-upstream origin $4-$3
+  # $1 - pipeline
+  # $2 - pipeline x.y
+  # $3 - community x.y 
+  # $4 - community branch name
+  # $5 - communtiy tag name
+  echo "Initializing $4 cutover:"
+  echo "$4 branch checkout"
+  git checkout master
+  git pull
+  git checkout -b $4-$3
+  echo "File Creation"
+  touch TAG
+  touch RELEASE_VERSION
+  echo $3 > RELEASE_VERSION
+  touch Z_RELEASE_VERSION
+  echo $3.0 > Z_RELEASE_VERSION
+  if [ "$1" = "release" ]; then
+    touch ACM_Z_RELEASE_VERSION
+    echo $2.0 > ACM_Z_RELEASE_VERSION
+    touch STOLOSTRON_ENGINE_RELEASE_VERSION
+    echo $3 > STOLOSTRON_ENGINE_RELEASE_VERSION
+  elif [ "$1" = "backplane" ]; then
+    touch BACKPLANE_Z_RELEASE_VERSION
+    echo $2.0 > BACKPLANE_Z_RELEASE_VERSION
+  fi
+  echo "Pushing to origin"
+  git add -A
+  git commit -am "Inital cutover for new branch"
+  git push --set-upstream origin $4-$3
 }
 
 operate() {
@@ -57,31 +62,38 @@ operate() {
   # $3 - communtiy branch x.y
   # $4 - community name
   # $5 - optional backplane version
+  echo "Initializing $1 cutover:"
+  echo "$1 branch checkout"
+  git checkout master
   git pull
   git checkout -b $1-$2
+  echo "File Creation"
   touch TAG
   touch RELEASE_VERSION
-  cat $2 > RELEASE_VERSION
+  echo $2 > RELEASE_VERSION
   touch Z_RELEASE_VERSION
-  cat $2.0 > Z_RELEASE_VERSION
+  echo $2.0 > Z_RELEASE_VERSION
   touch ${4}_RELEASE_VERSION
-  cat $3 > ${4}_RELEASE_VERSION
+  echo $3 > ${4}_RELEASE_VERSION
   if [ ! -z $5 ]; then
-    touch BACKPLANE_RELEASE_FILE
-    cat $5 > BACKPLANE_RELEASE_FILE
+    touch BACKPLANE_RELEASE_VERSION
+    echo $5 > BACKPLANE_RELEASE_VERSION
+  fi
+  echo "Pushing to origin"
+  git add -A
   git commit -am "Inital cutover for new branch"
   git push --set-upstream origin $1-$2
 }
 
 setup
 if [ "$1" = "backplane" ]; then
-    operate $1 $2 $3 STOLOSTRON_ENGINE
-    community_creation $1 $2 $4 stolostron-engine
+  operate $1 $2 $3 STOLOSTRON_ENGINE
+  community_creation $1 $2 $4 stolostron-engine
   # branch creation
   # stolostron-engine setup
 elif [ "$1" = "release" ]; then
-    operate $1 $2 $3 STOLOSTRON $4
-    community_creation $1 $2 $4 stolostron
+  operate $1 $2 $3 STOLOSTRON $4
+  community_creation $1 $2 $4 stolostron
   # branch creation
   # add backplane files
   # stolostron setup
