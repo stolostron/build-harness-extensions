b7e8b7b9d738f95dcd32a4ba61f81ef25b5c4df9
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2026-02-03 16:06:28 -0500

    feat: wipe previous pipeline commits (#217)
    The commits from the previous branch aren't relevant to the new release.
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- modules/pipeline-manifest/bin/_new-pipeline.sh
+++ modules/pipeline-manifest/bin/_new-pipeline.sh
@@ -1,48 +1,53 @@
 #!/bin/sh
 
+set -e
+
 # $1 - repo (pipeline vs. backplane-pipeline)
 # $2 - existing branch x.y
 # $3 - to-be branch x.y
 
 TMPPLACE=newpipe-tmp
+GIT_DIR=$TMPPLACE/pipeline
+GIT="git -C $GIT_DIR"
 
-if [ -z "$1" -o -z "$2" -o -z "$3" ]; then echo "I need three parameters: repo (pipeline or backplane-pipeline), old branch x.y number, new branch x.y number."; exit 1;
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+  echo "I need three parameters: repo (pipeline or backplane-pipeline), old branch x.y number, new branch x.y number."
+  exit 1
 fi
 
-if [ -d "$TMPPLACE" ]; then echo "The directory $TMPPLACE exists.  Remove it in order to continue."; exit 1;
+if [ -d "$TMPPLACE" ]; then
+  echo "The directory $TMPPLACE exists.  Remove it in order to continue."
+  exit 1
 fi
 
 setup() {
   # $1 - pipeline/backplane-pipeline repo
   mkdir $TMPPLACE
-  cd $TMPPLACE
-  git clone git@github.com:stolostron/$1 $TMPPLACE
-  cd $TMPPLACE
-}
-
-teardown() {
-  cd ../..
+  git clone "https://github.com/stolostron/$1" "$GIT_DIR"
+  $GIT fetch --all
 }
 
 operate() {
   # $1 - existing branch x.y
   # $2 - to-be branch x.y
   # $3 - branch suffix
-  git checkout $1-$3
-  git pull
-  git checkout -b $2-$3
-  rm snapshots/*
-  touch snapshots/.gitkeep
-  git commit -am "Clean out snapshots for new branch"
-  git push --set-upstream origin $2-$3
+  $GIT checkout "$1-$3"
+  $GIT checkout --orphan "$2-$3"
+  rm $GIT_DIR/snapshots/.gitkeep || true
+  if [ -d "$GIT_DIR/snapshots" ] && [ -z "$(find $GIT_DIR/snapshots -maxdepth 0 -empty)" ]; then
+    rm $GIT_DIR/snapshots/*
+  fi
+  touch $GIT_DIR/snapshots/.gitkeep
+  $GIT add .
+  $GIT commit -am "Initial commit for $2-$3"
+  $GIT push --set-upstream origin "$2-$3"
 }
 
-setup $1
-operate $2 $3 integration
-operate $2 $3 dev
-operate $2 $3 nightly
-operate $2 $3 preview
+setup "$1"
+operate "$2" "$3" integration
+operate "$2" "$3" dev
+operate "$2" "$3" nightly
+operate "$2" "$3" preview
 if [ "$1" = "pipeline" ]; then
-  operate $2 $3 stable
+  operate "$2" "$3" stable
 fi
-teardown
